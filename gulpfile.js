03edc56c492fd9d85879a050c83c2b8a107701f9
Nikhil Verma <nikhilnxvverma1@gmail.com>
2016-06-06 09:02:29 +0530

    Added gulp task for watching and transpiling typescript with sourcemaps. (done on dummy directories)

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var gulp        = require('gulp'),
 var shell = require('gulp-shell');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
+var ts=require('gulp-typescript');
+var sourcemaps=require('gulp-sourcemaps');
 
 var paths = {
   'src':['package.json']
@@ -15,9 +17,10 @@ var paths = {
     output: './css'
   },
   'typescript':{
-
+    src: './app-ts/**/*.ts',
+    output:'./app-js',
+    sourcemaps:'./sourcemaps'
   }
-
 };
 
 gulp.task('watch:sass', function () {
@@ -30,6 +33,22 @@ gulp.task('sass', function(){
       .pipe(gulp.dest(paths.style.output));
 });
 
+gulp.task('watch:typescript',function(){
+  gulp.watch(paths.typescript.src,['typescript']);
+});
+
+gulp.task('typescript',function(){
+  return gulp.src(paths.typescript.src)
+      .pipe(sourcemaps.init())
+      .pipe(ts({
+        noImplicitAny: false,
+        rootDir:'./app-ts',
+        outDir:'./app-js'
+      }))
+      .pipe(sourcemaps.write(paths.typescript.sourcemaps))
+      .pipe(gulp.dest(paths.typescript.output));
+});
+
 // create the gulp task
 gulp.task('run-no-electron-env', function () {
   childProcess.spawn(electron, ['--debug=5858','./app'], { stdio: 'inherit' });
@@ -37,4 +56,4 @@ gulp.task('run-no-electron-env', function () {
 
 gulp.task('run',shell.task('electron main.js'));
 
-gulp.task('default',['watch:sass','run']);
+gulp.task('default',['watch:sass','watch:typescript','run']);
