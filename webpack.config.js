c5cd0249ea138303d03ce857ecdaef50ae32611e
Nikhil Verma <nikhilnxvverma1@aol.com>
2017-05-27 23:22:44 +0530

    Webpack bugfixes pertaining to  module resolution

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,48 @@
+var webpack=require('webpack');
+var htmlWebpackPlugin=require('html-webpack-plugin');
+var helpers=require('./helpers');
+var path=require('path');
+
 module.exports={
 	entry:{
 		polyfills:'./src/polyfills.ts',
-		vendor:'./src/polyfills.ts',
 		app:'./src/main.ts',
 	},
-	rules:[
-		{
-			test:/\.ts$/,
-			loader:'awesome-typescript-loader'
-		},
-		{
-			test:/\.scss$/,
-			loaders:'style-loader!css-loader!sass-loader'
-		}
-	],
+	module:{
+		rules:[
+			{
+				test:/\.ts$/,
+				loader:'awesome-typescript-loader'
+			},
+			{
+				test:/\.scss$/,
+				loader:'style-loader!css-loader!sass-loader'
+			},
+			{
+				test:/\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+				loader:'file-loader',
+			}
+		],
+	},
+	resolve:{
+		modules:[path.resolve(__dirname,'src'),'node_modules'],
+		extensions:['.ts','.js','.json','.scss','.css','html'],
+	},
+	output:{
+		path:path.join(__dirname,'dist/'),
+		publicPath:path.join(__dirname,'dist/assets/'),
+		filename:'[name].bundle.js',
+	},
+	plugins:[
+		new webpack.ContextReplacementPlugin(
+			/angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+			__dirname
+		),
+		new webpack.optimize.CommonsChunkPlugin({
+			name:['app','polyfills']
+		}),
+		new htmlWebpackPlugin({
+			template:'./src/index.html'
+		})
+	]
 };
